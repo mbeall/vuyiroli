35ee8f667cfc4feb16d82b8d3f27a6e31be9ab34
Matt Beall <mbeall@starverte.com>
2014-02-23 19:21:48 -0700

    Create footer-close.php

---
+++ footer-close.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing elements
+ *
+ * @package Flint
+ * @sub-package Vuyiroli
+ * @since 0.1
+ */
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
